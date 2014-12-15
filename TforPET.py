caf57c7cd2e30b6ec9b0826540e1002926285d89
Mookhon <s7070033@kmitl.ac.th>
2014-12-15 21:14:14 +0700

    Revert "Successful"
    This reverts commit 2179d7c26c990eb15be747743e497e5681d2f30d.

--- TforPET.py
+++ TforPET.py
@@ -13,20 +13,18 @@ def temperature():
     
     #input checker
     for i in temp.get():
-        if i not in allowed or temp.get() == '-.' or temp.get() == '.-':
+        if i not in allowed:
             fault = 1
         elif i == '.':
             dot += 1
         elif i == '-':
             dash += 1
-        elif '-' in temp.get() and temp.get()[0] != '-':
-            fault = 1
             
     #input error
     if len(temp.get()) > 12:
         tkMessageBox.showinfo(message='Support Only 12 Digit.')
         return
-    elif fault == 1 or dot > 1 or dash > 1 or temp.get() == '.' or temp.get() == '-':
+    elif fault == 1 or dot > 1 or dash > 1 or temp.get() == '.':
         tkMessageBox.showinfo(message='Incorrect Input \n Please Try Again.')
         return
     elif temp.get() == '':
@@ -224,7 +222,8 @@ mLabel3 = Label(text='Faculty of Information Technology @ KMITL',bg='#FFFFFF').p
 
 #temperature input
 tlabel = Label(text='Input Temperature',fg='black',bg='#FFFFFF',font=tkFont.Font(size=12)).place(x=40,y=125)
-temp = Entry(mGui)
+var_temp = DoubleVar()
+temp = Entry(mGui, textvariable=var_temp)
 temp.pack(pady = 50)
 
 #box unit
